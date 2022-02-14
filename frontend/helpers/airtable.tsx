58c9a510dc387c4bf6adad8efd5cc1ccc0941a52
CorentinMartinot <corentin.martinot@gmail.com>
2022-02-14 12:33:10 +0100

    COM-2680 remove useless code

--- frontend/helpers/airtable.tsx
+++ frontend/helpers/airtable.tsx
@@ -1,20 +1,5 @@
 import { useRecords, useBase } from '@airtable/blocks/ui';
 
-
-export const useTableContent = (tableId: string) => {
-  const base = useBase();
-  const table = base.getTableById(tableId);
-  const records = useRecords(table);
-  const { fields } = table;
-
-  return records.map(
-    r => fields.reduce(
-      (acc, field) => ({ ...acc, [field.name]: r.getCellValue(field) }),
-      {}
-    )
-  );
-}
-
 export const useTableContentAsCsv = (tableId: string) => {
   const base = useBase();
   const table = base.getTableById(tableId);
--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,23 +1,12 @@
-import { initializeBlock, Button } from '@airtable/blocks/ui';
-import React, { useState } from 'react';
+import { initializeBlock, Button, Text, Loader } from '@airtable/blocks/ui';
+import React, { useEffect, useState } from 'react';
 import NiInput from './components/form/input';
-import { downloadCsv } from './helpers/files';
 import { downloadSEPAXml } from './helpers/sepa';
-import { getDateForFileName } from './helpers/dates';
-import { useTableContentAsCsv } from './helpers/airtable';
-import { ROOM_MATES_TABLE_ID } from '../.env/models';
 
 const App = () => {
-    const [amounts, setAmounts] = useState({ rent: 0, rentalExpenses: 0, currentExpenses: 0 });
-    const dataCsv = useTableContentAsCsv(ROOM_MATES_TABLE_ID);
-
-    const downloadCSV = () => {      
-        const filename = `prelevements_biens_communs_${getDateForFileName()}.csv`;
-        return downloadCsv(dataCsv, filename);
-    };
+    const [amounts, setAmounts] = useState({ rent: 1, rentalExpenses: 1, currentExpenses: 1 });
 
     const enableDownload = amounts.rent > 0 && amounts.rentalExpenses > 0 && amounts.currentExpenses > 0;
-
 
     const setAmountField = (key) => (e) => { setAmounts({ ...amounts, [key]: e.target.value }); };
     return <div>
@@ -26,7 +15,6 @@ const App = () => {
             label="Montant Charges locatives" required/>
         <NiInput value={amounts.currentExpenses} onChange={setAmountField('currentExpenses')}
             label="Montant Frais courants" required/>
-        <Button onClick={downloadCSV} icon="edit" disabled={!enableDownload}>Telecharger le CSV</Button>
         <Button onClick={downloadSEPAXml} icon="edit" disabled={!enableDownload}>Telecharger Le SEPA</Button>
     </div>;
 }
