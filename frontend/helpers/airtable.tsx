7ea8eb35dd89a344ae8595bd437636f2c2472baa
Manon Palin <manon@compani.fr>
2022-02-15 16:08:36 +0100

    COM-2681 - get Roommates data

--- frontend/helpers/airtable.tsx
+++ frontend/helpers/airtable.tsx
@@ -2,10 +2,16 @@ import { base } from '@airtable/blocks';
 import {
   CONFIG_TABLE_ID,
   CREDITOR_NAME_FIELD_ID,
-  ICS_FIELD_ID,
+  CREDITOR_ICS_FIELD_ID,
   CREDITOR_IBAN_FIELD_ID,
   CREDITOR_BIC_FIELD_ID,
   CREDITOR_PREFIX_FIELD_ID,
+  ROOMMATES_TABLE_ID,
+  ROOMMATE_FIELD_ID,
+  IBAN_FIELD_ID,
+  RUM_FIELD_ID,
+  BIC_FIELD_ID,
+  ICS_FIELD_ID,
 } from '../../.env/models';
 
 export const getConfig = async () => {
@@ -16,7 +22,7 @@ export const getConfig = async () => {
 
   const configs = {
     creditorName: configRecord.getCellValue(CREDITOR_NAME_FIELD_ID),
-    ics: configRecord.getCellValue(ICS_FIELD_ID),
+    ics: configRecord.getCellValue(CREDITOR_ICS_FIELD_ID),
     creditorIBAN: configRecord.getCellValue(CREDITOR_IBAN_FIELD_ID),
     creditorBIC: configRecord.getCellValue(CREDITOR_BIC_FIELD_ID),
     creditorPrefix: configRecord.getCellValue(CREDITOR_PREFIX_FIELD_ID),
@@ -25,3 +31,22 @@ export const getConfig = async () => {
   queryResult.unloadData();
   return configs;
 }
+
+export const getRoommatesData = async () => {
+  const roommatesTable = base.getTable(ROOMMATES_TABLE_ID);
+  const queryResult = await roommatesTable.selectRecordsAsync();
+  const roommatesData = [];
+
+  for (const record of queryResult.records) {
+    const roommateData = {
+      debitorName: record.getCellValue(ROOMMATE_FIELD_ID),
+      debitorIBAN: record.getCellValue(IBAN_FIELD_ID),
+      debitorRUM: record.getCellValue(RUM_FIELD_ID),
+      debitorBIC: record.getCellValue(BIC_FIELD_ID),
+      debitorICS: record.getCellValue(ICS_FIELD_ID),
+    };
+    roommatesData.push(roommateData);
+  }
+  
+  return roommatesData;
+};
