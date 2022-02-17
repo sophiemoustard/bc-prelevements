718e9d93ad133435c8ac23b0470003e9d4256c2a
CorentinMartinot <corentin.martinot@gmail.com>
2022-02-17 14:38:50 +0100

    COM-2686 fix ics validation

--- frontend/helpers/validations.tsx
+++ frontend/helpers/validations.tsx
@@ -4,6 +4,6 @@ export const isValidIBAN = value => _isValidIBAN(value?.split(' ').join(''));
 
 export const isValidBIC = _isValidBIC;
 
-export const isValidICS = value => value && /^[A-Z]{2}[0-9]{2}[A-Z]{3}[0-9]{6}$/.test(value);
+export const isValidICS = value => value && /^[A-Z]{2}[0-9]{2}[0-9A-Z]{3}[0-9A-Z]+$/.test(value);
 
 export const isValidPrefix = value => value && /^[0-9]{3}$/.test(value);
