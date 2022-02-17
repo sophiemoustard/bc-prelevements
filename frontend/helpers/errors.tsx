40a74d4bd0bd360d4a61896cfdad65f1d5a2abd6
CorentinMartinot <corentin.martinot@gmail.com>
2022-02-17 15:56:46 +0100

    COM-2686 refacto addMessageAndThrow

--- frontend/helpers/errors.tsx
+++ frontend/helpers/errors.tsx
@@ -4,9 +4,12 @@ export const throwValidationError = (message) => {
   const error = new Error(message)
   error.name = VALIDATION_ERROR;
   throw error;
-}
+};
 
-export const addMessageAndThrow = (e, addedMessage) => {
-  if (e.name !== VALIDATION_ERROR) e.message = e.message.concat('\n\t', addedMessage);
+export const addMessageAndThrow = (error, addedMessage) => {
+  const e = new Error();
+  e.name = error.name;
+  e.message = error.name === VALIDATION_ERROR ? error.message : error.message.concat('\n\t', addedMessage);
+
   throw e;
-}
+};
