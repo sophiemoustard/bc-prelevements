aa8026e7f96cabe6d6e992374fda4e78680cde27
Manon Palin <manon@compani.fr>
2022-02-25 17:45:07 +0100

    COM-2706 - remove useless conditions and add - in RUM regex

--- frontend/helpers/validations.tsx
+++ frontend/helpers/validations.tsx
@@ -7,12 +7,12 @@ export const isValidIBAN = value => _isValidIBAN(value?.split(' ').join(''));
 
 export const isValidBIC = _isValidBIC;
 
-export const isValidICS = value => value && /^[A-Z]{2}[0-9]{2}[0-9A-Z]{3}[0-9A-Z]+$/.test(value);
+export const isValidICS = value => /^[A-Z]{2}[0-9]{2}[0-9A-Z]{3}[0-9A-Z]+$/.test(value);
 
-export const isValidPrefix = value => value && /^[0-9]{3}$/.test(value);
+export const isValidPrefix = value => /^[0-9]{3}$/.test(value);
 
 export const isValidTransactionLabel = value => value.length <= 140; 
 
-export const isValidRUM = value => value && /^[0-9A-Z]{1,35}$/.test(value);
+export const isValidRUM = value => /^[/\-/0-9A-Z]{1,35}$/.test(value);
 
 export const isValidDate = value => value && dayjs(value).isValid();
